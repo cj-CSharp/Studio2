8c3dc2f701b93d269a9266cfab166eed2def19bb
Carol Connor <cjallen1988@gmail.com>
2020-09-20 13:41:54 -0500

    first commit

---
+++ Studio2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
